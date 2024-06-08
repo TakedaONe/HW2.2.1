fd671a030d740f4de2726fbc34222a67fe06bcbc
TakedaONe <146793652+TakedaONe@users.noreply.github.com>
2024-06-08 16:19:41 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount();
+        account.deposit(15000);
+
+        try {
+            while (true) {
+                account.withDraw(6000);
+                System.out.println("Успешно снято 6000 сом. Остаток: " + account.getAmount());
+            }
+        } catch (LimitException e) {
+            try {
+                account.withDraw((int) e.getRemainingAmount());
+                System.out.println("Снято остаток: " + e.getRemainingAmount() + ". Остаток: " + account.getAmount());
+            } catch (LimitException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        System.out.println("Конечный остаток на счете: " + account.getAmount());
     }
 }
